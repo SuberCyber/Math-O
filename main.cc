367ef7364240ae6142de17d783b8c345387573ad
mfsybers <mfsybers@student.cs.uwaterloo.ca>
2019-09-06 16:41:29 -0400

    Colour changes and cheating prevention methods

--- main.cc
+++ main.cc
@@ -9,13 +9,14 @@ using namespace std;
 int main() {
     string i;
     int wrong = 0;
+    int guesses = 0;
 
     cout << "\u001b[40;1m\u001b[37;1mEnter password:" << endl;
 
     while (cin >> i){
+        ++ guesses;
         // number to match
         if (i == "1047"){
-            cout << "\u001b[42mCorrect!\u001b[40;1m" << endl;
             break;
         } else {
             cout << "\u001b[1A\u001b[2K";
@@ -26,33 +27,52 @@ int main() {
         if (wrong >= 4 && (wrong % 4  == 0)){
             cout << "Time is ticking!" << endl;
         }
+
     }
-    
+     while (guesses >= -1){
+        cout << "\u001b[1A\u001b[2K \u001b[1A\u001b[2K ";
+        --guesses;
+    }
+    cout << endl << endl << endl << endl << "\u001b[42mCorrect!\u001b[40;1m" << endl;
+
+
     Riddles r;
-    
+
     cout << endl;
 
     Mastermind mm;
     mm.playGame();
-   
-    
+
+
     cout << endl << endl; 
     cout << endl << endl;
     cout << "            \u001b[35;1m     UULL" << endl;
     cout << endl << endl;
-    cout << "                     ..-. .- ... - \u001b[0m" << endl;  
+    cout << "           -  ..  .  ...  \u001b[0m;1m" << endl;  
     cout << endl << endl;
 
     cout << "Please enter Poke-password:" << endl;
 
+    guesses = 0;
+
     while (cin >> i){
+        ++ guesses;   
         if (i == "help"){
-        cout << endl << endl;
-        cout << "congradulatuions for helping the professors!" << endl;
+            cout << endl << endl;
+            break;
         }
         else {
-        cout << "that is not the password" << endl;
+            cout << "that is not the password" << endl;
         }
     }
+
+    while (guesses >= -10){
+        cout << "\u001b[1A\u001b[2K \u001b[1A\u001b[2K ";
+        --guesses;
+    }
+
+    cout << endl << endl << endl << endl << endl << endl << endl << "\u001b[35mCongratulations for helping the professors!" << endl;
+    cout << endl << endl << endl; 
+
     return 0;
 }
